98bbed7064ebbbccbbea9a087a1b71f801f80746
vjcitn <stvjc@channing.harvard.edu>
2021-10-26 12:21:36 -0400

    minor updates

--- R/tests.R
+++ R/tests.R
@@ -32,3 +32,26 @@ anv_fhir_test_find_patient = function(url = anvurl(), id = "H_WX-999-999", pyref
   pp = pyrefs$fhirclient$models$patient$Patient
   pp$where(reticulate::py_dict("identifier", id))$perform_resources(sm$server)
 }
+
+#' URL for fhirStores FHIR structure
+#' @export
+test_stores_url = function() 'https://healthcare.googleapis.com/v1/projects/gcp-testing-308520/locations/us-east4/datasets/testset/fhirStores/fhirstore/fhir'
+
+#' retrieve references to all stores
+#' @param url character(1) URL for a FHIR store collection
+#' @param count_str character(1) `_count` value for `where` in ResearchStudy
+#' @param pyrefs list of Module references (anvil, fhirclient)
+#' @examples 
+#' allst = anv_fhir_get_stores()
+#' length(allst)
+#' head(sapply(allst, function(x) x$title))
+#' @export
+anv_fhir_get_stores = function(url=test_stores_url(), count_str = "1000", pyrefs = abfhir_demo()) {
+  sm = connect_smart(
+        fhirclient = pyrefs$anvil$fhir$client$FHIRClient, 
+        fhirurl = url) 
+  stopifnot(sm$ready)
+  rs = pyrefs$fhirclient$models$researchstudy
+  rs$ResearchStudy$where(py_dict("_count", count_str))$perform_resources(smartBase$server)
+}
+
