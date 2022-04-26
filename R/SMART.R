427f141d96428c00200ba172947e4d1083ad9ddb
vjcitn <stvjc@channing.harvard.edu>
2022-04-26 13:55:53 -0400

    update to use pyAnVIL==0.1.0rc6

--- R/SMART.R
+++ R/SMART.R
@@ -1,5 +1,6 @@
 #' long URL string for server announced at https://the-anvil.slack.com/archives/C02HU4GT04B/p1634236625021500
 #' @param tag character(1) name of store, defaults to "public"
+#' @note need to ensure project_tag is appropriate over time
 #' @export
 anvurl = function (tag="public", project_tag = "fhir-test-16-342800") 
 sprintf("https://healthcare.googleapis.com/v1beta1/projects/%s/locations/us-west2/datasets/anvil-test/fhirStores/%s/fhir", project_tag, tag)
@@ -14,10 +15,14 @@ asymurl = function() "https://healthcare.googleapis.com/v1/projects/gcp-testing-
 #' we use `$anvil$fhir$client$FHIRClient`, populating the `settings` field
 #' @examples
 #' dem = abfhir_demo()
-#' sm = connect_smart(dem$anvil$fhir$client$FHIRClient) # clumsy!
+#' sm = connect_smart(dem$anvil$fhir$client$DispatchingFHIRClient) # can handle list of stores
 #' sm$ready
+#' avail_resch = dem$fhirclient$models$researchstudy
+#' res = avail_resch$ResearchStudy$where(reticulate::py_dict("", ""))$perform_resources(sm$server)
+#' length(res)
+#' unlist(lapply(res[1:5], function(x) x$id))
 #' @export
-connect_smart = function(fhirclient, fhirurl = asymurl(), app_id="my_app") {
+connect_smart = function(fhirclient, fhirurls = list(anvurl()), app_id="my_app") {
 #
 # FIXME: Here is where a list of 'stores' can be used to prepare access to 
 # many resources, and a shiny app would be useful.  Consider three levels
@@ -25,7 +30,7 @@ connect_smart = function(fhirclient, fhirurl = asymurl(), app_id="my_app") {
 # which connects app with investigational objectives, and "user/programmer"
 # running interactive sessions in Rstudio
 #
-   settings = list(app_id = app_id, api_bases = list(fhirurl))
+   settings = list(app_id = app_id, api_bases = fhirurls)
    sm = fhirclient(settings = settings)
    if (!isTRUE(sm$ready)) message("'ready' was not TRUE, may need to retry when server warms up")
    sm
